3b94630640fd2d2eeb5deeecaab50236dda29cfa
kimkonpig <linggooling@gmail.com>
2020-11-26 11:14:58 +0900

    201126 modified

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.Member2;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.Team;
 
 import javax.persistence.EntityManager;
@@ -12,31 +14,6 @@ import java.util.List;
 public class JpaMain {
     public static void main(String[] args){
 
-        //연관관계 매핑 기초 강의 직전 코드(Member.class 사용)
-        /*EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
-        EntityManager em = emf.createEntityManager();
-
-        EntityTransaction tx = em.getTransaction();
-        tx.begin();
-
-        try{
-            Order order = em.find(Order.class, 1L);
-            Long memberId = order.getMemberId();
-
-            Member member = em.find(Member.class, memberId);
-
-            Member findMember = order.getMember();
-
-            tx.commit();
-        } catch (Exception e){
-            tx.rollback();
-        } finally {
-            em.close();
-        }
-
-        emf.close();*/
-
         //연관관계 매핑 기초 - 단방향 연관관계 코드(Member2.class 사용)
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
@@ -44,42 +21,15 @@ public class JpaMain {
         tx.begin();
 
         try{
-            //저장
-            Team team = new Team();
-            team.setName("TeamA");
-            //team.getMembers().add(member2);
-            em.persist(team);
+            /*Order order = new Order(); //주문 객체 생성
+            order.addOrderItem(new OrderItem()); //원하는 orderItem 추가*/
 
-            Member2 member2 = new Member2();
-            member2.setUsername("member1");
-            //member2.changeTeam(team); // setTeam()에서 changeTeam()으로 이름 변경
-            em.persist(member2);
+            Order order = new Order();
+            em.persist(order);
 
-            //Team.java setTeam에 편의 메소드로 생성하여 주석 처리
-            //team.getMembers().add(member2);
-            
-            //영속성 컨텍스트 비워내고 초기화
-            em.flush();
-            em.clear();
-
-            Team findTeam = em.find(Team.class, team.getId());
-            List<Member2> member2s = findTeam.getMembers();
-
-            for(Member2 m : member2s){
-                System.out.println("m = " + m.getUsername());
-            }
-
-
-            /*Member2 findMember = em.find(Member2.class, member2.getId());
-            *//*Team findTeam = findMember.getTeam();
-            System.out.println("findTeamName = " + findTeam.getName());*//*
-
-            List<Member2> members = findMember.getTeam().getMembers();
-            //member -> team -> member
-
-            for(Member2 m : members){
-                System.out.println("m = " + m.getUsername());
-            }*/
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+            em.persist(orderItem);
 
             tx.commit();
         }catch(Exception e){
